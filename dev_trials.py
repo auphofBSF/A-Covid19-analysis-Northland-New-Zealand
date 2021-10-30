a27fe4930e1d12f7f35976acccb4e961a13a2458
Anthony Uphof <anthony@smartlife-labs.com>
2021-10-31 08:57:54 +1300

    feat: wip custom graph - vac status

--- dev_trials.py
+++ dev_trials.py
@@ -39,10 +39,94 @@ data.prep_step_2_read_all_uptake_data()
 data.prep_step_3_generate_report_data()
 
 # %%
-data.df_compare.dtypes
 # %%
-data.weekly_data_folders.keys()
+import modules.data_viz1 as dv
 
+
+# %%
+dv.plot2(data.df_all_weeks_no_gender_age_category)
+
+# %%
+ethnic_group = "European or Other"
+df = data.df_all_weeks_no_gender_age_category
+# %%
+df
+# %%
+fig = px.bar(
+    df.loc[df["Ethnic group"] == ethnic_group],
+    x="Week ending",
+    y="First dose administered Changed",
+    title=f"{ethnic_group} -  First Dose",
+    color="Age Category",
+    hover_data=[
+        "Ethnic group",
+        "Age Category",
+        # "Population unvaccinated at week start",
+        "Population unvaccinated at week end",
+        "Population",
+        "First dose administered_prior",
+        "First dose administered Changed",
+        "First dose administered Changed as Percent of unvaccinated Population",
+    ],
+)
+fig
+# %%
+fig = px.bar(
+    df.loc[df["Ethnic group"] == ethnic_group],
+    x="Week ending",
+    y="First dose administered",
+    title=f"{ethnic_group} -  First Dose",
+    color="Age Category",
+    hover_data=[
+        "Ethnic group",
+        "Age Category",
+        # "Population unvaccinated at week start",
+        "Population unvaccinated at week end",
+        "Population",
+        "First dose administered_prior",
+        "First dose administered Changed",
+        "First dose administered Changed as Percent of unvaccinated Population",
+    ],
+)
+fig
+# %%
+fig = px.bar(
+    df.loc[df["Ethnic group"] == ethnic_group],
+    x="Week ending",
+    y="Second dose administered",
+    title=f"{ethnic_group} -  Second Dose",
+    color="Age Category",
+    hover_data=[
+        "Ethnic group",
+        "Age Category",
+        # "Population unvaccinated at week start",
+        "Population unvaccinated at week end",
+        "Population",
+        "First dose administered_prior",
+        "First dose administered Changed",
+        "First dose administered Changed as Percent of unvaccinated Population",
+    ],
+)
+fig
+# %%
+fig = px.bar(
+    df.loc[df["Ethnic group"] == ethnic_group],
+    x="Week ending",
+    y="Population",
+    title=f"{ethnic_group} -  Population",
+    color="Age Category",
+    hover_data=[
+        "Ethnic group",
+        "Age Category",
+        # "Population unvaccinated at week start",
+        "Population unvaccinated at week end",
+        "Population",
+        "First dose administered_prior",
+        "First dose administered Changed",
+        "First dose administered Changed as Percent of unvaccinated Population",
+    ],
+)
+fig
 # %%
 
 fig = px.scatter(
@@ -65,19 +149,31 @@ fig = px.scatter(
 fig
 # %%
 
+import altair as alt 
+# %%
+chart1 = alt.Chart(data.df_all_weeks.query('`Age group`=="12-15" & `Gender`=="Male"')).mark_line().encode(
+   x="Week ending",
+   y="Population unvaccinated at week start",
+   color="Ethnic group"
+).properties(
+   height=300, width=500
+)
+chart2 = alt.Chart(data.df_all_weeks.query('`Age group`=="12-15" & `Gender`=="Female"')).mark_line().encode(
+   x="Week ending",
+   y="Population unvaccinated at week start",
+   color="Ethnic group"
+).properties(
+   height=300, width=500
+)
 
-
-
-
-
-
+# %%
+chart1 | chart2
 
 
 
 
 # %%
 fig = px.line(data.df_all_weeks.loc[data.df_all_weeks['Age group']=='12-15'], x="Week ending", y="Population unvaccinated at week start", color='Ethnic group',facet_col="Gender",facet_row="Age group")
-# %%
 fig
 # %%
 fig = px.line(data.df_all_weeks.loc[data.df_all_weeks['Ethnic group']=="European or Other"], x="Week ending", y="Population unvaccinated at week end", color='Age group',facet_col="Gender")
@@ -97,6 +193,298 @@ fig.update_layout(
     yaxis={'title':'UnVaccinated Population'})
 
 fig
+# %%
+fig = px.area(data.df_all_weeks_no_gender_age_category.query('["European or Other", "Maori"] in `Ethnic group`'
+                    )
+                    # .sort_values(["Week ending", "Age Category","Ethnic group"])
+        , 
+        x="Week ending", y="Second dose administered", color='Age Category',
+        title="Total Maori and European Second Dosed",
+        markers=True,
+        line_group="Ethnic group",
+        # facet_col="Gender"
+        )
+fig.update_layout(
+    yaxis={'title':'Second Dosed Population'})
+fig
+# %%
+"""[summary]
+
+"""
+# %%
+df = data.df_all_weeks_no_gender_age_category.query((
+    '["European or Other", "Maori"] in `Ethnic group`'
+    ' and `Age Category` == "12-19"'
+    )
+)
+
+# %%
+import plotly.graph_objects as go
+
+# x=sorted(df['Week ending'].unique())
+x=df['Week ending'].unique()
+# %%
+fig = None
+fig = go.Figure()
+
+ethnic_group="Maori"
+age_category="12-19"
+y_offset=0
+set_stack_group="one"
+
+def fig_section(fig,ethnic_group,age_category,y_offset=0,section='',
+    colors=['#FD3216','#00FE35','#6A76FC']):
+    vac_status="None"
+
+
+    def fig_section_trace(
+        fig,df,ethnic_group,age_category,field='',y_offset=0,y_multiplier=1,section='',
+        color=None,showlegend=True,**kwargs):
+
+        if showlegend:
+            kwargs = {**kwargs, 
+                      **dict(
+                       legendgroup=section,
+                        legendgrouptitle=dict(
+                                text=f"{ethnic_group}"
+                        ),
+                      )
+            }
+        print(f"kwargs: {kwargs}")
+        fig.add_trace(go.Scatter(
+            name=f"{age_category} - {vac_status}",
+            x=x, 
+            y=y_offset+df.query(f'["{ethnic_group}"] in `Ethnic group`'
+                )[field].astype('Int64')*y_multiplier,
+            
+            mode='lines',
+            line=dict(width=0.5, 
+                        color=color
+                        # color='rgb(184, 247, 212)'
+            ),
+            showlegend=showlegend,
+            # fillcolor=colors[0],
+            # linecolor=colors[0],
+            hoveron = 'points+fills', # select where hover is active
+            text=f"{ethnic_group}<br>{age_category} - {vac_status}",
+            hoverinfo = 'text',
+
+            marker = dict(
+                autocolorscale=False,
+                cauto=False,
+                color=colors[0]
+                        ),
+            stackgroup=section,
+            # groupnorm='' # sets the normalization for the sum of the stackgroup
+            **kwargs
+        ))
+
+
+    if y_offset>0:
+        fig_section_trace(
+                    fig,
+                    df,
+                    field='Population unvaccinated at week end',
+                    ethnic_group=ethnic_group,
+                    age_category=age_category,
+                    y_offset=y_offset,
+                    y_multiplier=0,
+                    section=section,
+                    color=colors[0], 
+                    showlegend=False,
+                    fill='none')
+        # fig.add_trace(go.Scatter(
+        #         name=f"{age_category} - {vac_status}",
+        #         x=x, y=y_offset+df.query(f'["{ethnic_group}"] in `Ethnic group`'
+        #             )['Population unvaccinated at week end'].astype('Int64')*0,
+        #         fill='none',
+        #         mode='lines',
+        #         line=dict(width=0.5, 
+        #                     color=colors[0]
+        #                     # color='rgb(184, 247, 212)'
+        #         ),
+        #         showlegend=False,
+        #         # legendgroup=section,
+        #         # legendgrouptitle=dict(
+        #         #     text=f"{ethnic_group}"
+
+        #         # ),
+        #         # fillcolor=colors[0],
+        #         # linecolor=colors[0],
+        #         hoveron = 'points+fills', # select where hover is active
+        #         text=f"{ethnic_group}<br>{age_category} - {vac_status}",
+        #         hoverinfo = 'text',
+
+        #         marker = dict(
+        #             autocolorscale=False,
+        #             cauto=False,
+        #             color=colors[0]
+        #                     ),
+        #         stackgroup=section,
+        #         # groupnorm='' # sets the normalization for the sum of the stackgroup
+        #     ))
+    fig_section_trace(
+                fig,
+                df,
+                field='Population unvaccinated at week end',
+                ethnic_group=ethnic_group,
+                age_category=age_category,
+                y_offset=0,
+                y_multiplier=1,
+                section=section,
+                color=colors[0], 
+                # - extra trace config
+                groupnorm=''
+    )
+    # fig.add_trace(go.Scatter(
+    #     name=f"{age_category} - {vac_status}",
+    #     x=x, y=df.query(f'["{ethnic_group}"] in `Ethnic group`'
+    #         )['Population unvaccinated at week end'].astype('Int64'),
+    #     mode='lines',
+    #     line=dict(width=0.5, 
+    #                 color=colors[0]
+    #                 # color='rgb(184, 247, 212)'
+    #     ),
+    #     legendgroup=section,
+    #     legendgrouptitle=dict(
+    #         text=f"{ethnic_group}"
+
+    #     ),
+    #     # fillcolor=colors[0],
+    #     # linecolor=colors[0],
+    #     hoveron = 'points+fills', # select where hover is active
+    #     text=f"{ethnic_group}<br>{age_category} - {vac_status}",
+    #     hoverinfo = 'text',
+    #     marker = dict(
+    #         autocolorscale=False,
+    #         cauto=False,
+    #         color=colors[0]
+    #                 ),
+    #     stackgroup=section,
+    #     groupnorm='' # sets the normalization for the sum of the stackgroup
+    # ))
+
+    vac_status="First Dose"
+    fig_section_trace(
+                fig,
+                df,
+                field='First dose administered',
+                ethnic_group=ethnic_group,
+                age_category=age_category,
+                y_offset=0,
+                y_multiplier=1,
+                section=section,
+                color=colors[1], 
+                # - extra trace config
+    )
+
+    # fig.add_trace(go.Scatter(
+    #     name=f"{age_category} - {vac_status}",
+    #     x=x, y=df.query(f'["{ethnic_group}"] in `Ethnic group`'
+    #             )['First dose administered'].astype('Int64'),
+    #     mode='lines',
+    #     line=dict(width=0.5, 
+    #                 color=colors[1]
+    #                 # color='rgb(111, 231, 219)'
+    #     ),
+    #     # fillcolor=colors[1],
+    #     # linecolor=colors[1],
+    #     hoveron = 'points+fills', # select where hover is active
+    #     text=f"{ethnic_group}<br>{age_category} - {vac_status}",
+    #     hoverinfo = 'text',
+    #     stackgroup=section,
+    #     marker = dict(
+    #         autocolorscale=False,
+    #         cauto=False,
+    #         color=colors[1]
+    #         ),
+
+    # ))
+    vac_status="Second Dose"
+    fig_section_trace(
+                fig,
+                df,
+                field='Second dose administered',
+                ethnic_group=ethnic_group,
+                age_category=age_category,
+                y_offset=0,
+                y_multiplier=-1,
+                section=section,
+                color=colors[2], 
+                # - extra trace config
+                fill="tonexty",
+    )
+    # fig.add_trace(go.Scatter(
+    #     name=f"{age_category} - {vac_status}",
+    #     x=x, y=-df.query(f'["{ethnic_group}"] in `Ethnic group`'
+    #             )['Second dose administered'].astype('Int64'),
+    #     mode='lines',
+    #     fill="tonexty",
+    #     line=dict(width=0.5, 
+    #                 color=colors[2]
+    #                 # color='rgb(127, 166, 238)'
+    #     ),
+    #     # fillcolor=colors[2],
+    #     # linecolor=colors[2],
+    #     hoveron = 'points+fills', # select where hover is active
+    #     text=f"{ethnic_group}<br>{age_category} - {vac_status}",
+    #     hoverinfo = 'text',
+    #     marker = dict(
+    #         autocolorscale=False,
+    #         cauto=False,
+    #         color=colors[2]
+    #     ),
+
+    #     stackgroup=section
+    # ))
+
+fig_section(fig,ethnic_group="Maori",age_category="12-19",
+            y_offset=0,section='one',
+            colors=['#AD3216','#F0FE35','#3A76FC'],
+            )
+fig_section(fig,ethnic_group="European or Other",age_category="12-19",
+            y_offset=9714,
+            # y_offset=df.query('["Maori"] in `Ethnic group`'
+            # )['Population'].max(),
+            section='two',
+            colors=['#FD3246','#00FE75','#6A762C'],
+            )
+
+
+fig.update_layout(
+    showlegend=True,
+    xaxis=dict(
+        autorange=True,
+        type='date',
+    ),
+    yaxis=dict(
+        type='linear',
+        range=[0, 40000],
+        ticksuffix=''))
+
+fig
+#%%
+# fig.add_trace(go.Scatter(
+#     x=x, y=[100, 100, 100, 100],
+#     mode='lines',
+#     line=dict(width=0.5, color='rgb(131, 90, 241)'),
+#     stackgroup='one'
+# ))
+
+fig.update_layout(
+    showlegend=True,
+    xaxis=dict(
+        autorange=True,
+        type='date',
+    ),
+    yaxis=dict(
+        type='linear',
+        range=[0, 40000],
+        ticksuffix=''))
+
+fig
+
+
 # %%
 fig = px.area(data.df_all_weeks_no_gender_age_category.loc[:], 
         x="Week ending", y="Population unvaccinated at week end", color='Age Category',
